7afa54a557baa6c8574bf12c2e4d0f4dc5add6c6
Omkar Wadhavane <omkarwadhavane1000@gmail.com>
2026-08-09 12:01:51 +0530

    Add new files

---
+++ src/types/pdfjs-dist.d.ts
@@ -0,0 +1,6 @@
+declare module 'pdfjs-dist/webpack.mjs' {
+  export function getDocument(src: any): any;
+  export const GlobalWorkerOptions: any;
+  const defaultExport: any;
+  export default defaultExport;
+}
